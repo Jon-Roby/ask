3ec4d615dbda7b07f63c3d094cd6f247a0d38dd0
Jon Roby <jonathan.roby1@gmail.com>
2015-08-05 13:08:26 -0700

    Create skeleton

---
+++ client/templates/questions/questions_list.js
@@ -0,0 +1,17 @@
+var questionsData = [
+  {
+    title: 'Introducing Telescope',
+    url: 'http://sachagreif.com/introducing-telescope/'
+  },
+  {
+    title: 'Meteor',
+    url: 'http://meteor.com'
+  },
+  {
+    title: 'The Meteor Book',
+    url: 'http://themeteorbook.com'
+  }
+];
+Template.questionsList.helpers({
+  questions: questionsData
+});
